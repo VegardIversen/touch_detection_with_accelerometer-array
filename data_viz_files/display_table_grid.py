d2a49403b897f4ef2acf50f5e0172b299489d64a
Niklas Strømsnes <niklas.stromsnes@revolve.no>
2022-09-26 12:43:51 +0200

    Color a desired grid spot given by a coordinate

--- data_viz_files/display_table_grid.py
+++ data_viz_files/display_table_grid.py
@@ -1,66 +1,90 @@
 import sys
 import pygame
+from pygame.locals import KEYDOWN, K_q
 
 # CONSTANTS:
-SCREENSIZE = WIDTH, HEIGHT = 600, 400
+SCREENSIZE = WIDTH, HEIGHT = 325, 325
 BLACK = (0, 0, 0)
 GREY = (160, 160, 160)
 WHITE = (255, 255, 255)
-PADDING = PADTOPBOTTOM, PADLEFTRIGHT = 60, 60
+CONTAINER_WIDTH_HEIGHT = 300  # Not to be confused with SCREENSIZE
+CONT_X, CONT_Y = 10, 10  # TOP LEFT OF CONTAINER
 # VARS:
 _VARS = {'surf': False}
 
 
 def main():
-    "Draws a grid of squares on the screen."
     pygame.init()
     _VARS['surf'] = pygame.display.set_mode(SCREENSIZE)
     while True:
+        checkEvents()
         _VARS['surf'].fill(WHITE)
-        drawGrid(3)
+        drawGrid(divisions = 3)
+        drawRect(divisions = 3, row = 2, col = 2)
         pygame.display.update()
 
 
+# Draw filled rectangle at coordinates x,y 18,18 with size width,height
+# 60,60
+def drawRect(divisions, row, col):
+    pygame.draw.rect(
+     _VARS['surf'], BLACK,
+     (CONT_X + row * CONTAINER_WIDTH_HEIGHT / divisions + (0.1 * CONTAINER_WIDTH_HEIGHT / divisions),
+     CONT_Y + col * CONTAINER_WIDTH_HEIGHT / divisions + (0.1 * CONTAINER_WIDTH_HEIGHT / divisions), 
+     0.8 * CONTAINER_WIDTH_HEIGHT / divisions,
+     0.8 * CONTAINER_WIDTH_HEIGHT / divisions)
+    )
+
+
 def drawGrid(divisions):
-    # DRAW Rectangle
+
+    # Get cell size, just one since its a square grid.
+    cellSize = CONTAINER_WIDTH_HEIGHT/divisions
+
+    # DRAW Grid Border:
     # TOP lEFT TO RIGHT
     pygame.draw.line(
       _VARS['surf'], BLACK,
-      (0 + PADLEFTRIGHT, 0 + PADTOPBOTTOM),
-      (WIDTH - PADLEFTRIGHT, 0 + PADTOPBOTTOM), 2)
-    # BOTTOM lEFT TO RIGHT
+      (CONT_X, CONT_Y),
+      (CONTAINER_WIDTH_HEIGHT + CONT_X, CONT_Y), 2)
+    # # BOTTOM lEFT TO RIGHT
     pygame.draw.line(
       _VARS['surf'], BLACK,
-      (0 + PADLEFTRIGHT, HEIGHT - PADTOPBOTTOM),
-      (WIDTH - PADLEFTRIGHT, HEIGHT - PADTOPBOTTOM), 2)
-    # LEFT TOP TO BOTTOM
+      (CONT_X, CONTAINER_WIDTH_HEIGHT + CONT_Y),
+      (CONTAINER_WIDTH_HEIGHT + CONT_X, CONTAINER_WIDTH_HEIGHT + CONT_Y), 2)
+    # # LEFT TOP TO BOTTOM
     pygame.draw.line(
       _VARS['surf'], BLACK,
-      (0 + PADLEFTRIGHT, 0 + PADTOPBOTTOM),
-      (0 + PADLEFTRIGHT, HEIGHT - PADTOPBOTTOM), 2)
-    # RIGHT TOP TO BOTTOM
+      (CONT_X, CONT_Y),
+      (CONT_X, CONT_Y + CONTAINER_WIDTH_HEIGHT), 2)
+    # # RIGHT TOP TO BOTTOM
     pygame.draw.line(
       _VARS['surf'], BLACK,
-      (WIDTH - PADLEFTRIGHT, 0 + PADTOPBOTTOM),
-      (WIDTH - PADLEFTRIGHT, HEIGHT - PADTOPBOTTOM), 2)
+      (CONTAINER_WIDTH_HEIGHT + CONT_X, CONT_Y),
+      (CONTAINER_WIDTH_HEIGHT + CONT_X, CONTAINER_WIDTH_HEIGHT + CONT_Y), 2)
 
-    # Get cell size
-    horizontal_cellsize = (WIDTH - (PADLEFTRIGHT * 2)) / divisions
-    vertical_cellsize = (HEIGHT - (PADTOPBOTTOM * 2)) / divisions
 
     # VERTICAL DIVISIONS: (0,1,2) for grid(3) for example
     for x in range(divisions):
         pygame.draw.line(
            _VARS['surf'], BLACK,
-           (0 + PADLEFTRIGHT + (horizontal_cellsize * x), 0 + PADTOPBOTTOM),
-           (0 + PADLEFTRIGHT + horizontal_cellsize * x, HEIGHT - PADTOPBOTTOM), 2)
-    # HORITZONTAL DIVISION
+           (CONT_X + (cellSize * x), CONT_Y),
+           (CONT_X + (cellSize * x), CONTAINER_WIDTH_HEIGHT + CONT_Y), 2)
+    # # HORIZONTAl DIVISIONS
         pygame.draw.line(
           _VARS['surf'], BLACK,
-          (0 + PADLEFTRIGHT, 0 + PADTOPBOTTOM + (vertical_cellsize * x)),
-          (WIDTH - PADLEFTRIGHT, 0 + PADTOPBOTTOM + (vertical_cellsize * x)), 2)
+          (CONT_X, CONT_Y + (cellSize * x)),
+          (CONT_X + CONTAINER_WIDTH_HEIGHT, CONT_Y + (cellSize * x)), 2)
+
+
+def checkEvents():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == KEYDOWN and event.key == K_q:
+            pygame.quit()
+            sys.exit()
 
 
 if __name__ == '__main__':
     main()
-    
