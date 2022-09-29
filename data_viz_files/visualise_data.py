386a24d9fb16e8e949b3e8857229dbe16f9075f9
Niklas Strømsnes <niklas.stromsnes@revolve.no>
2022-09-29 16:22:25 +0200

    stuff

--- data_viz_files/visualise_data.py
+++ data_viz_files/visualise_data.py
@@ -6,8 +6,6 @@ import scipy
 from scipy import signal
 
 
-
-
 def crop_data(data, crop_mode):
     """CROP_MODE:
     Manual,
@@ -21,6 +19,7 @@ def crop_data(data, crop_mode):
 
     return data_cropped
 
+
 def plot_fft(df, Fs=150000, window=False):
     
     if window:
@@ -28,12 +27,12 @@ def plot_fft(df, Fs=150000, window=False):
         data_fft = scipy.fft.fft(df.values * hamming_window)
     else:
         data_fft = scipy.fft.fft(df.values, axis=0)
-    
-    #print(df.values)
-    
-    fftfreq = scipy.fft.fftfreq(len(data_fft),1/Fs)
-    N = int(len(data_fft)/2)
-    #fft_x_axis = np.linspace(0,(Fs/ 2),N)
+
+    # print(df.values)
+
+    fftfreq = scipy.fft.fftfreq(len(data_fft),  1/Fs)
+    N = int(len(data_fft) / 2)
+    # fft_x_axis = np.linspace(0,(Fs/ 2),N)
     plt.grid()
     plt.title('fft of signal')
     plt.xlabel("Frequency [hz]")
@@ -48,6 +47,7 @@ def plot_fft(df, Fs=150000, window=False):
 def plot_fft_with_hamming(df, Fs=80000):    
     plot_fft(df, window=True)
 
+
 def plot_data(df, crop=True):
     if crop:
         df = crop_data(df, CROP_MODE)
@@ -59,26 +59,28 @@ def plot_data(df, crop=True):
 
 
 def plot_spectogram(df, include_signal=True, sample_rate=150000, channel='channel 1', freq_max=None):
-    
+
     if include_signal:
-        time_axis = np.linspace(0, len(df)//sample_rate, num=len(df))
+        time_axis = np.linspace(0, len(df) // sample_rate, num=len(df))
         ax1 = plt.subplot(211)
+        plt.grid()
         plt.plot(time_axis, df[channel])
         plt.xlabel('Time')
         plt.ylabel('Amplitude')
         ax2 = plt.subplot(212, sharex=ax1)
-        plt.specgram(df[channel],Fs=sample_rate)
+        plt.specgram(df[channel], Fs=sample_rate)
         plt.axis(ymax=freq_max)
         plt.xlabel('Time')
         plt.ylabel('Frequency')
     else:
-        plt.specgram(df[channel],Fs=sample_rate)
+        plt.specgram(df[channel], Fs=sample_rate)
         plt.axis(ymax=freq_max)
         plt.xlabel('Time')
         plt.ylabel('Frequency')
     plt.show()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
 
     # Config 
     SAMPLE_RATE = 150000     # Hz
@@ -91,7 +93,7 @@ if __name__=='__main__':
 
     data_folder = f'{Path.home()}\\OneDrive - NTNU\\NTNU\\ProsjektOppgave'
 
-    test_file = data_folder + '\\fingernail_test_passive_setup2\\touch_test_fingernail_passive_setup2_place_A1_center_v2.csv'
+    test_file = data_folder + '\\first_test_touch_active_setup2_5\\touch_test_active_setup2_5_B2_v1.csv'
     print(test_file)
     df = pd.read_csv(test_file, delimiter=DATA_DELIMITER, names=['channel 1', 'channel 2', 'channel 3'] )
     #print(df.head())
