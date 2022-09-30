788fe9f37f77a132d0b0d80150202667db181bf0
Niklas Strømsnes <niklas.stromsnes@revolve.no>
2022-09-30 11:02:54 +0200

    making linter happy

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-print(__file__)
 from data_processing.cross_correlation_position import FindTouchPosition
 from data_viz_files.display_table_grid import draw
 from pathlib import Path
 
+print(__file__)
+
 DATA_FOLDER = f'{Path.home()}\\OneDrive - NTNU\\NTNU\\ProsjektOppgave'
 
-if __name__=='__main__':
-    cell = FindTouchPosition(DATA_FOLDER+ '\\first_test_touch_passive_setup2\\touch_test_passive_setup2_place_B3_center_v2.csv')
+if __name__ == '__main__':
+    cell = FindTouchPosition(DATA_FOLDER + '\\first_test_touch_passive_setup2\\touch_test_passive_setup2_place_B3_center_v2.csv')
     draw(cell)
