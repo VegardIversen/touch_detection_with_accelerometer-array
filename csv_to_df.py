000fb6c982f90b26deb3bc906d3a25663d252581
Niklas Strømsnes <niklas.stromsnes@revolve.no>
2022-10-06 22:02:14 +0200

    update csv_to_df()

--- csv_to_df.py
+++ csv_to_df.py
@@ -3,12 +3,18 @@ from pathlib import Path
 
 
 def csv_to_df(file_folder, file_name, channel_names=['channel 1', 'channel 2', 'channel 3']):
-    """Make a DataFrame from a .csv file"""
+    """Returns a DataFrame from a .csv file.
+    Set channel_names to None to return a
+    DataFrame with the default column names.
+    """
 
     ROOT_FOLDER = f'{Path.home()}\\OneDrive - NTNU\\NTNU\\ProsjektOppgave'
     file_path = ROOT_FOLDER + '\\' + file_folder + '\\' + file_name + '.csv'
-    df = pd.read_csv(file_path, names=channel_names)
-    print("\n" + "Using data file path:", file_path)
+    df = pd.read_csv(filepath_or_buffer=file_path, names=channel_names)
+    print("\n" + 'Creating DataFrame from file folder "' + file_folder + '" and file name "' + file_name + '"\n')
 
     return df
 
+
+if __name__ == '__main__':
+    csv_to_df("base_data", "df_average_noise")
