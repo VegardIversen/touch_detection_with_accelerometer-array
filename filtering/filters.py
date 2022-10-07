a55d633fdfe2243f8ee7eb5d18c00879859afff9
Niklas Strømsnes <niklas.stromsnes@revolve.no>
2022-10-08 01:05:24 +0200

    move filter_signal() to filter file

--- filtering/filters.py
+++ filtering/filters.py
@@ -14,3 +14,21 @@ def low_pass_filter(sig, cutoff=1000, fs=150000, order=5):
     b, a = signal.butter(order, cutoff / (fs / 2), btype='lowpass')
     filtered_sig = signal.filtfilt(b, a, sig)
     return filtered_sig
+
+
+def filter_signal(sig, freqs, sample_rate=150000):
+    """Input an array of frequencies <freqs> to filter out
+    with a Q factor given by an array of <Qs>.
+    """
+    for freq in freqs:
+        # We want smaller q-factors for higher frequencies
+        q = freq ** (1 / 3)
+        b_notch, a_notch = signal.iirnotch(freq / (0.5 * sample_rate), q)
+        sig_filtered = sig
+
+        for channel in sig_filtered:
+            sig_filtered[channel] = signal.filtfilt(b_notch,
+                                                    a_notch,
+                                                    sig[channel].values)
+
+    return sig_filtered
