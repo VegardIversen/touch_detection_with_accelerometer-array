e4aa1db725829e101493efd6c277941e91cb8d76
Niklas Strømsnes <niklas.stromsnes@revolve.no>
2022-10-10 09:24:18 +0200

    mess around with hilbert in a quest to find echoes

---
+++ data_processing/detect_echoes.py
@@ -0,0 +1,49 @@
+from symbol import file_input
+from threading import TIMEOUT_MAX
+from time import time
+import numpy as np
+import scipy.signal as signal
+import matplotlib.pyplot as plt
+from csv_to_df import csv_to_df
+from data_viz_files.visualise_data import crop_data
+from filtering.filters import high_pass_filter, low_pass_filter
+
+
+def find_indices_of_peaks(sig, plot=False):
+    """Find the indices of the peaks in the signal."""
+    sig_np = sig.to_numpy()
+    # Crop the signal
+    sig_np_filtered = high_pass_filter(sig_np, cutoff=1000, order=5)
+    sig_np_filtered_hilbert = get_hilbert_envelope(sig_np_filtered)
+    # peak_indices = signal.argrelmax(sig_np_filtered_hilbert, order=10000)[0]
+    # peak_indices = signal.find_peaks(sig_np_filtered_hilbert, height=0.0005, threshold=0.0001)[0]
+    peak_indices = signal.find_peaks_cwt(sig_np_filtered_hilbert, widths=5)
+    if peak_indices.size == 0:
+        print('No peaks found!')
+
+    if plot:
+        time_axis = np.linspace(0, len(sig_np), num=len(sig_np))
+        fig, ax0 = plt.subplots(nrows=1)
+        # ax0.plot(time_axis, sig_np, label='signal')
+        ax0.plot(time_axis / 150000, sig_np_filtered, label='filtered')
+        ax0.plot(time_axis / 150000, sig_np_filtered_hilbert, label='filtered hilbert')
+        ax0.plot(time_axis[peak_indices] / 150000, sig_np_filtered_hilbert[peak_indices], 'x', label='peaks')
+        ax0.set_xlabel("Time [s]")
+        ax0.legend()
+        fig.tight_layout()
+        plt.grid()
+        plt.show()
+
+    return peak_indices
+
+
+def get_hilbert_envelope(sig_np):
+    analytic_signal = signal.hilbert(sig_np)
+    amplitude_envelope = np.abs(analytic_signal)
+    return amplitude_envelope
+
+
+if __name__ == '__main__':
+    signal_df = csv_to_df(file_folder='first_test_touch_passive_setup2',
+                          file_name='touch_test_passive_setup2_place_C3_center_v2')
+    find_indices_of_peaks(signal_df)
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from data_viz_files.visualise_data import compare_signals
+from data_viz_files.visualise_data import compare_signals, crop_data, crop_data_threshold, plot_data_vs_noiseavg
 from data_processing.noise import adaptive_filter_RLS, adaptive_filter_NLMS, noise_reduce_signal
-import pandas as pd
 from csv_to_df import csv_to_df
 from data_processing.find_propagation_speed import find_propagation_speed
+from data_processing.detect_delays import find_indices_of_peaks
 
 
 print('\n' + __file__ + '\n')
@@ -11,29 +11,18 @@ print('\n' + __file__ + '\n')
 SAMPLE_RATE = 150000
 
 # Crop limits in seconds
-TIME_START = 0
-TIME_END = 5
+TIME_START = 1.46
+TIME_END = 2
 
 
 def main():
-    chirp_df = csv_to_df(file_folder='div_files',
-                         file_name='chirp_test_fs_150000_t_max_2s_1000-40000hz_1vpp_1cyc_setup3_v1')
+    signal_df = csv_to_df(file_folder='first_test_touch_passive_setup2',
+                          file_name='touch_test_passive_setup2_place_C3_center_v2')
 
-    find_propagation_speed(chirp_df, sr=SAMPLE_RATE)
+    signal_df = crop_data(signal_df, time_start=TIME_START, time_end=TIME_END)
 
-    # noise_df = csv_to_df(file_folder='base_data',
-    #                      file_name='df_average_noise',
-    #                      channel_names=None)
+    find_indices_of_peaks(signal_df['channel 1'], plot=True)
 
-    # df = csv_to_df(file_folder='first_test_touch_passive_setup2',
-    #                file_name='touch_test_passive_setup2_place_A1_center_v1')
-
-    # reduced_noise = noise_reduce_signal(df['channel 1'], noise_df['channel 1'])
-
-    # compare_signals(df['channel 1'],
-    #                 pd.Series(reduced_noise, copy=False),
-    #                 save=True,
-    #                 filename='signal_and_reduced_nois_signal.png')
 
 
 if __name__ == '__main__':
