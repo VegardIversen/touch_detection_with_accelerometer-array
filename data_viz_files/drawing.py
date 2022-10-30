ae2c1bcab7da4a8784f080337238a3bccab6469c
Niklas Strømsnes <niklas.stromsnes@revolve.no>
2022-10-30 18:14:51 +0100

    Classes be classy

--- data_viz_files/drawing.py
+++ data_viz_files/drawing.py
@@ -4,6 +4,16 @@ import matplotlib.patches as patches
 from collections import OrderedDict
 
 
+class Table:
+    """Represents the table and its edges."""
+    LENGTH = 0.716    # m
+    WIDTH = 0.597     # m
+    # Enum for representing edges:
+    TOP_EDGE = 1
+    RIGHT_EDGE = 2
+    BOTTOM_EDGE = 3
+    LEFT_EDGE = 4
+
 def draw_table(plot_lines=True):
     "Draw the table with the real dimensions, including the lines."
     TABLE_LENGTH = 0.716    # m
@@ -12,8 +22,8 @@ def draw_table(plot_lines=True):
     TABLE_LINE_COLOUR = '#f0c18b'
 
     table = patches.Rectangle((0, 0),
-                              TABLE_LENGTH,
-                              TABLE_WIDTH,
+                              Table.LENGTH,
+                              Table.WIDTH,
                               fc=TABLE_SURFACE_COLOUR,
                               ec=TABLE_LINE_COLOUR,
                               lw=2,
@@ -23,14 +33,14 @@ def draw_table(plot_lines=True):
 
     if plot_lines:
         for i in range(1, 3):
-            line_x = plt.Line2D((i * TABLE_LENGTH / 3, i * TABLE_LENGTH / 3),
-                                (0, TABLE_WIDTH),
+            line_x = plt.Line2D((i * Table.LENGTH / 3, i * Table.LENGTH / 3),
+                                (0, Table.WIDTH),
                                 color=TABLE_LINE_COLOUR,
                                 lw=0.75,
                                 linestyle='--',
                                 zorder=1)
-            line_y = plt.Line2D((0, TABLE_LENGTH),
-                                (i * TABLE_WIDTH / 3, i * TABLE_WIDTH / 3),
+            line_y = plt.Line2D((0, Table.LENGTH),
+                                (i * Table.WIDTH / 3, i * Table.WIDTH / 3),
                                 color=TABLE_LINE_COLOUR,
                                 lw=0.75,
                                 linestyle='--',
@@ -95,23 +105,19 @@ def flip_and_draw_sources(source_coords: np.array,
 
     for edge in edges_to_flip_around:
         for source_coord in source_coords:
-            if edge == 1:
-                # Top edge of the table
-                new_coord = source_coord + np.array([0, 2 * (TABLE_WIDTH - source_coord[1])])
+            if edge == Table.TOP_EDGE:
+                new_coord = source_coord + np.array([0, 2 * (Table.WIDTH - source_coord[1])])
                 source_coords = np.vstack((source_coords, new_coord))
                 draw_actuator(new_coord)
-            elif edge == 2:
-                # Right edge of the table
-                new_coord = source_coord + np.array([2 * (TABLE_LENGTH - source_coord[0]), 0])
+            elif edge == Table.RIGHT_EDGE:
+                new_coord = source_coord + np.array([2 * (Table.LENGTH - source_coord[0]), 0])
                 source_coords = np.vstack((source_coords, new_coord))
                 draw_actuator(new_coord)
-            elif edge == 3:
-                # Bottom edge of the table
+            elif edge == Table.BOTTOM_EDGE:
                 new_coord = source_coord + np.array([0, -2 * source_coord[1]])
                 source_coords = np.vstack((source_coords, new_coord))
                 draw_actuator(new_coord)
-            elif edge == 4:
-                # Left edge of the table
+            elif edge == Table.LEFT_EDGE:
                 new_coord = source_coord + np.array([-2 * source_coord[0], 0])
                 source_coords = np.vstack((source_coords, new_coord))
                 draw_actuator(new_coord)
@@ -135,23 +141,19 @@ def flip_and_draw_sensors(sensor_coords,
 
     for edge in edges_to_flip_around:
         for sensor_coord in sensor_coords:
-            if edge == 1:
-                # Top edge of the table
-                new_coord = sensor_coord + np.array([0, 2 * (TABLE_WIDTH - sensor_coord[1])])
+            if edge == Table.TOP_EDGE:
+                new_coord = sensor_coord + np.array([0, 2 * (Table.WIDTH - sensor_coord[1])])
                 sensor_coords = np.vstack((sensor_coords, new_coord))
                 draw_sensor(new_coord)
-            elif edge == 2:
-                # Right edge of the table
-                new_coord = sensor_coord + np.array([2 * (TABLE_LENGTH - sensor_coord[0]), 0])
+            elif edge == Table.RIGHT_EDGE:
+                new_coord = sensor_coord + np.array([2 * (Table.LENGTH - sensor_coord[0]), 0])
                 sensor_coords = np.vstack((sensor_coords, new_coord))
                 draw_sensor(new_coord)
-            elif edge == 3:
-                # Bottom edge of the table
+            elif edge == Table.BOTTOM_EDGE:
                 new_coord = sensor_coord + np.array([0, -2 * sensor_coord[1]])
                 sensor_coords = np.vstack((sensor_coords, new_coord))
                 draw_sensor(new_coord)
-            elif edge == 4:
-                # Left edge of the table
+            elif edge == Table.LEFT_EDGE:
                 new_coord = sensor_coord + np.array([-2 * sensor_coord[0], 0])
                 sensor_coords = np.vstack((sensor_coords, new_coord))
                 draw_sensor(new_coord)
@@ -165,21 +167,20 @@ def plot_legend_without_duplicates():
 
 
 def draw_setup_1():
-    TABLE_LENGTH = 0.716    # m
-    TABLE_WIDTH = 0.597     # m
-
     plt.axes()
     draw_table()
 
-    actuator_coord = np.array([TABLE_LENGTH / 2, 1 * TABLE_WIDTH / 6])
+    actuator_coord = np.array([Table.LENGTH / 2, 1 * Table.WIDTH / 6])
     draw_actuator(actuator_coord)
 
-    sensor_2_coord = np.array([TABLE_LENGTH / 2, TABLE_WIDTH - 0.05])
-    sensor_1_coord = sensor_2_coord + np.array([-0.08 / 2, -(np.sqrt(0.08 ** 2 - 0.04 ** 2))])
-    sensor_3_coord = sensor_2_coord + np.array([0.08 / 2, -(np.sqrt(0.08 ** 2 - 0.04 ** 2))])
-    draw_sensor(sensor_2_coord)
-    draw_sensor(sensor_1_coord)
-    draw_sensor(sensor_3_coord)
+    SENSOR_2_COORD = np.array([Table.LENGTH / 2, Table.WIDTH - 0.05])
+    SENSOR_2_OFFSET = np.array([-0.08 / 2, -(np.sqrt(0.08 ** 2 - 0.04 ** 2))])
+    SENSOR_2_COORD = SENSOR_2_COORD + SENSOR_2_OFFSET
+    SENSOR_3_OFFSET = np.array([0.08 / 2, -(np.sqrt(0.08 ** 2 - 0.04 ** 2))])
+    SENSOR_3_COORD = SENSOR_2_COORD + SENSOR_3_OFFSET
+    draw_sensor(SENSOR_2_COORD)
+    draw_sensor(SENSOR_2_COORD)
+    draw_sensor(SENSOR_3_COORD)
 
     plt.axis('scaled')
     plt.xlabel('x (m)')
@@ -190,14 +191,12 @@ def draw_setup_1():
 
 
 def draw_a_setup():
-    TABLE_LENGTH = 0.716    # m
-    TABLE_WIDTH = 0.597     # m
-    ACTUATOR_COORD = np.array([TABLE_LENGTH / 6,
-                               TABLE_WIDTH / 2])
-    SENSOR_1_COORD = np.array([2 * TABLE_LENGTH / 3,
-                               5 * TABLE_WIDTH / 6])
-    SENSOR_2_COORD = np.array([2 * TABLE_LENGTH / 3,
-                               1 * TABLE_WIDTH / 3])
+    ACTUATOR_COORD = np.array([Table.LENGTH / 6,
+                               Table.WIDTH / 2])
+    SENSOR_1_COORD = np.array([2 * Table.LENGTH / 3,
+                               5 * Table.WIDTH / 6])
+    SENSOR_2_COORD = np.array([2 * Table.LENGTH / 3,
+                               1 * Table.WIDTH / 3])
 
     source_coords = np.array([ACTUATOR_COORD])
     sensor_coords = np.array([SENSOR_1_COORD])
@@ -209,7 +208,7 @@ def draw_a_setup():
     # Draw each sensor_coord in sensor_coords
     [draw_sensor(sensor_coord) for sensor_coord in sensor_coords]
 
-    EDGES_TO_FLIP_AROUND = np.array([1, 3])
+    EDGES_TO_FLIP_AROUND = np.array([Table.TOP_EDGE, Table.BOTTOM_EDGE])
 
     flip_and_draw_sensors(sensor_coords, EDGES_TO_FLIP_AROUND)
     flip_and_draw_sources(source_coords, EDGES_TO_FLIP_AROUND)
