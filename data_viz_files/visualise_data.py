cc5a1685aef537e0611c0f95548aa519d60db3fa
Niklas Strømsnes <niklas.stromsnes@revolve.no>
2022-11-02 10:22:08 +0100

    Function if customizing font sizes are desired

--- data_viz_files/visualise_data.py
+++ data_viz_files/visualise_data.py
@@ -301,5 +301,19 @@ def plot_data_sub_ffts(df, channel='channel 1', sample_rate=150000):
     # compare_signals(df[channel], noise_df[channel])
 
 
+def set_fontsizes():
+    SMALL_SIZE = 15
+    MEDIUM_SIZE = 20
+    BIGGER_SIZE = 25
+
+    plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+    plt.rc('axes', titlesize=MEDIUM_SIZE)     # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+
 if __name__ == '__main__':
     pass
