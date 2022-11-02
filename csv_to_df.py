e4f2366863ce57687d4e9c8e22ae1056a5cada63
Niklas Strømsnes <niklas.stromsnes@revolve.no>
2022-11-02 10:21:47 +0100

    Use fstrings

--- csv_to_df.py
+++ csv_to_df.py
@@ -10,13 +10,13 @@ def csv_to_df(file_folder, file_name, channel_names=['channel 1', 'channel 2', '
     """
     if platform == 'linux':
         ROOT_FOLDER = '/mnt/c/Users/nikla/OneDrive - NTNU/NTNU/ProsjektOppgave'
-        file_path = ROOT_FOLDER + '/' + file_folder + '/' + file_name + '.csv'
+        file_path = f'{ROOT_FOLDER}/{file_folder}/{file_name}.csv'
     else:
         ROOT_FOLDER = f'{Path.home()}\\OneDrive - NTNU\\NTNU\\ProsjektOppgave'
-        file_path = ROOT_FOLDER + '\\' + file_folder + '\\' + file_name + '.csv'
+        file_path = f'{ROOT_FOLDER}\\{file_folder}\\{file_name}.csv'
 
     df = pd.read_csv(filepath_or_buffer=file_path, names=channel_names)
-    print("\n" + 'Creating DataFrame from file folder "' + file_folder + '" and file name "' + file_name + '"\n')
+    print('\n' + f'Creating DataFrame from file folder "{file_folder}" and file name "{file_name}' + '\n')
 
     return df
 
