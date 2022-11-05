8f78a1c543c17a794066c7461b0862e7b3cf034b
Niklas Strømsnes <niklas.stromsnes@revolve.no>
2022-11-05 14:09:00 +0100

    Setups are now classes instead of functions

--- data_viz_files/drawing.py
+++ data_viz_files/drawing.py
@@ -1,4 +1,3 @@
-from unicodedata import mirrored
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
@@ -8,148 +7,11 @@ from data_processing.detect_echoes import find_mirrored_source, flip_sensors, fl
 from objects import MirroredSensor, MirroredSource, Table, Actuator, Sensor
 
 
-def draw_table(plot_lines=True):
-    "Draw the table with the real dimensions, including the lines."
-    table = patches.Rectangle((0, 0),
-                              Table.LENGTH,
-                              Table.WIDTH,
-                              fc=Table.SURFACE_COLOUR,
-                              ec=Table.LINE_COLOUR,
-                              lw=2,
-                              zorder=0)
-
-    plt.gca().add_patch(table)
-
-    if plot_lines:
-        for i in range(1, 3):
-            line_x = plt.Line2D((i / 3 * Table.LENGTH, i / 3 * Table.LENGTH),
-                                (0, Table.WIDTH),
-                                color=Table.LINE_COLOUR,
-                                lw=0.75,
-                                linestyle='--',
-                                zorder=1)
-            line_y = plt.Line2D((0, Table.LENGTH),
-                                (i / 3 * Table.WIDTH, i / 3 * Table.WIDTH),
-                                color=Table.LINE_COLOUR,
-                                lw=0.75,
-                                linestyle='--',
-                                zorder=1)
-            plt.gca().add_patch(line_x)
-            plt.gca().add_patch(line_y)
-
-
-def draw_actuator(actuator_coord: np.array):
-
-    actuator = plt.Circle(actuator_coord,
-                          radius=Actuator.RADIUS,
-                          fc=Actuator.FILL_COLOUR,
-                          ec=Actuator.EDGE_COLOUR,
-                          label='Actuator/mirrored source')
-    plt.gca().add_patch(actuator)
-
-
-def draw_sensor(actuator_coord):
-    SENSOR_COLOUR = '#AEAFA7'
-    sensor = plt.Circle(actuator_coord,
-                        radius=0.01,
-                        fc=SENSOR_COLOUR,
-                        ec='dimgray',
-                        label='Sensor')
-    plt.gca().add_patch(sensor)
-
-
-def draw_waves(actuator_coord: np.array, n_waves=3):
-    """Draw arcs representing the waves.
-    TODO:   Automate to have the correct wavelength (and amplitude?)
-            for a given frequency. Might also be better with circles.
-    """
-    ARC_COLOUR = '#f0c18b'
-    for i in range(1, n_waves + 1):
-        arc = patches.Arc(actuator_coord,
-                          0.07 * i,     # could be automated with wavelengths
-                          0.07 * i,     # could be automated with wavelengths
-                          theta1=0,
-                          theta2=180,
-                          color=ARC_COLOUR,
-                          lw=0.75,
-                          linestyle='--')
-        plt.gca().add_patch(arc)
-
-
 def plot_legend_without_duplicates():
     """Avoid duplicate labels in the legend"""
     handles, labels = plt.gca().get_legend_handles_labels()
     by_label = OrderedDict(zip(labels, handles))
     plt.legend(by_label.values(), by_label.keys(), loc='upper right')
-
-
-def draw_arrow(start_coord, end_coord, subtract_length=0):
-    """Draw an arrow between two points.
-    The arrow is drawn from the start_coord to the end_coord.
-    """
-    ARROW_SENSOR_OFFSET = end_coord - start_coord
-    plt.arrow(start_coord[0], start_coord[1],
-              ARROW_SENSOR_OFFSET[0], ARROW_SENSOR_OFFSET[1],
-              color='black',
-              head_width=0.03,
-              length_includes_head=True)
-
-
-def draw_setup_2():
-    table = Table()
-
-    actuator = Actuator(coordinates=np.array([1 / 2 * table.LENGTH,
-                                              1 / 6 * table.WIDTH]))
-
-    sensor_2 = Sensor(coordinates=np.array([table.LENGTH / 2,
-                                            table.WIDTH - 0.082]),
-                                            radius = 0.013)
-    SENSOR_1_OFFSET = np.array([-0.08 / 2, -(np.sqrt(0.08 ** 2 - 0.04 ** 2))])
-    SENSOR_3_OFFSET = np.array([0.08 / 2, -(np.sqrt(0.08 ** 2 - 0.04 ** 2))])
-    sensor_1 = Sensor(coordinates=(sensor_2.coordinates + SENSOR_1_OFFSET))
-    sensor_3 = Sensor(coordinates=(sensor_2.coordinates + SENSOR_3_OFFSET))
-
-    plt.axes()
-    table.draw()
-    actuator.draw()
-    sensor_2.draw()
-    sensor_1.draw()
-    sensor_3.draw()
-
-    plt.axis('scaled')
-    plt.xlabel('x (m)')
-    plt.ylabel('y (m)')
-    plot_legend_without_duplicates()
-
-    plt.show()
-
-    return actuator, sensor_1, sensor_2, sensor_3
-
-
-def draw_setup_3_2():
-    table = Table()
-
-    sensor_1 = Sensor(coordinates=np.array([0.135, 0.305]))
-    sensor_2 = Sensor(sensor_1.coordinates + np.array([0.267, 0]), radius=0.013)
-    sensor_3 = Sensor(sensor_2.coordinates + np.array([0.267, 0]))
-
-    actuator = Actuator(np.array([sensor_1.x / 2, sensor_1.y]))
-
-    plt.axes()
-    table.draw()
-    actuator.draw()
-    sensor_1.draw()
-    sensor_2.draw()
-    sensor_3.draw()
-
-    plt.axis('scaled')
-    plt.xlabel('x (m)')
-    plt.ylabel('y (m)')
-    plot_legend_without_duplicates()
-
-    plt.show()
-
-    return actuator, np.array([sensor_1, sensor_2, sensor_3])
 
 
 def draw_setup_ideal():
---
+++ setups.py
@@ -0,0 +1,96 @@
+import numpy as np
+import pandas as pd
+import scipy.signal as signal
+import matplotlib.pyplot as plt
+
+from constants import SAMPLE_RATE
+from objects import MirroredSensor, MirroredSource, Table, Actuator, Sensor
+
+from data_viz_files.drawing import plot_legend_without_duplicates
+from data_processing.detect_echoes import find_mirrored_source, flip_sensors, flip_sources
+
+
+class Setup:
+    table = Table()
+    actuators = np.array([])
+    sensors = np.array([])
+
+    def __init__(self):
+        raise NotImplementedError("Setup version needs to be specified, e.g. Setup2")
+
+    def draw(self):
+        plt.axes()
+        self.table.draw()
+        [actuator.draw() for actuator in self.actuators]
+        [sensor.draw() for sensor in self.sensors]
+        plt.axis('scaled')
+        plt.xlabel('x (m)')
+        plt.ylabel('y (m)')
+        plot_legend_without_duplicates()
+        plt.show()
+
+    def get_objects(self):
+        return self.actuators, self.sensors
+
+
+class Setup2(Setup):
+    def __init__(self):
+        self.actuator = Actuator(coordinates=np.array([1 / 2 * self.table.LENGTH,
+                                                       1 / 9 * self.table.WIDTH]))
+        self.actuators = np.append(self.actuators, self.actuator)
+
+        self.sensor_2 = Sensor(coordinates=np.array([self.table.LENGTH / 2,
+                                                     self.table.WIDTH - 0.082]),
+                               radius=0.013)
+        SENSOR_1_OFFSET = np.array([-0.08 / 2, -(np.sqrt(0.08 ** 2 - 0.04 ** 2))])
+        SENSOR_3_OFFSET = np.array([0.08 / 2, -(np.sqrt(0.08 ** 2 - 0.04 ** 2))])
+        self.sensor_1 = Sensor((self.sensor_2.coordinates + SENSOR_1_OFFSET))
+        self.sensor_3 = Sensor((self.sensor_2.coordinates + SENSOR_3_OFFSET))
+        self.sensors = np.append(self.sensors, self.sensor_1)
+        self.sensors = np.append(self.sensors, self.sensor_2)
+        self.sensors = np.append(self.sensors, self.sensor_3)
+
+    def get_propagation_speed(self, df1: pd.DataFrame, df2: pd.DataFrame):
+        """Use the cross correlation between the two channels
+        to find the propagation speed. Based on:
+        https://stackoverflow.com/questions/41492882/find-time-shift-of-two-signals-using-cross-correlation
+        """
+        n = len(df1)
+        """Convert to df if np.ndarray"""
+        if type(df1) == np.ndarray:
+            df1 = pd.DataFrame(df1)
+        if type(df2) == np.ndarray:
+            df2 = pd.DataFrame(df2)
+
+        corr = signal.correlate(df1, df2, mode='same') \
+               / np.sqrt(signal.correlate(df2, df2, mode='same')[int(n / 2)]
+               * signal.correlate(df1, df1, mode='same')[int(n / 2)])
+
+        delay_arr = np.linspace(-0.5 * n / SAMPLE_RATE, 0.5 * n / SAMPLE_RATE, n)
+        delay = delay_arr[np.argmax(corr)]
+        distance = np.abs(self.sensor_1.y - self.sensor_2.y)
+        propagation_speed = np.round(np.abs(distance / delay), decimals=2)
+        return propagation_speed
+
+
+class Setup3_2(Setup):
+    def __init__(self):
+        self.sensor_1 = Sensor(coordinates=np.array([0.135, 0.305]))
+        self.sensor_2 = Sensor(self.sensor_1.coordinates + np.array([0.267, 0]), radius=0.013)
+        self.sensor_3 = Sensor(self.sensor_2.coordinates + np.array([0.267, 0]))
+        self.sensors = np.append(self.sensors, self.sensor_1)
+        self.sensors = np.append(self.sensors, self.sensor_2)
+        self.sensors = np.append(self.sensors, self.sensor_3)
+        self.actuator = Actuator(np.array([self.sensor_1.x / 2, self.sensor_1.y]))
+        self.actuators = np.append(self.actuators, self.actuator)
+
+
+class Setup3_4(Setup3_2):
+    def __init__(self):
+        super().__init__()
+        """Exact location of the actuator is not remembered,
+        but it is one of these two.
+        """
+        # self.actuator.set_coordinates(np.array([0.01, self.actuator.y]))
+        self.sensor_3.set_coordinates(np.array([self.table.LENGTH - 0.009,
+                                                self.sensor_3.y]))
