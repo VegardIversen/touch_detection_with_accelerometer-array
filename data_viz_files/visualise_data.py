0a3d958d27b14549cdab3fa3f58191ea48c69642
Niklas Strømsnes <niklas.stromsnes@revolve.no>
2022-11-12 17:11:53 +0100

    Sync y axises as well

--- data_viz_files/visualise_data.py
+++ data_viz_files/visualise_data.py
@@ -12,6 +12,69 @@ from data_processing.detect_echoes import get_hilbert_envelope
 from data_viz_files.drawing import plot_legend_without_duplicates
 
 
+def compare_signals(fig, axs,
+                    data: list,
+                    freq_max: int = 40000,
+                    nfft: int = 256):
+    """Visually compare two signals, by plotting:
+    time signal, spectogram, fft and (optionally) difference signal
+    """
+    for i, channel in enumerate(data):
+        if isinstance(data[i], np.ndarray):
+            data[i] = pd.Series(data[i], name='Sensor ' + str(i + 1))
+        """Time signal"""
+        time_axis = np.linspace(start=0,
+                                stop=len(data[i]) / SAMPLE_RATE,
+                                num=len(data[i]))
+        axs[i, 0].sharex(axs[0, 0])
+        axs[i, 0].sharey(axs[0, 0])
+        axs[i, 0].grid()
+        axs[i, 0].plot(time_axis, data[i])
+        axs[i, 0].set_title(f'{data[i].name}, time signal')
+        axs[i, 0].set_xlabel('Time [s]')
+        axs[i, 0].set_ylabel('Amplitude [V]')
+        axs[i, 0].plot()
+
+        """Spectrogram"""
+        spec = axs[i, 1].specgram(data[i],
+                                  Fs=SAMPLE_RATE,
+                                  NFFT=nfft,
+                                  noverlap=(nfft // 2))
+        # fig.colorbar(spec[3], ax=axs[i, 1])
+        spec[3].set_clim(10 * np.log10(np.max(spec[0])) - 60,
+                         10 * np.log10(np.max(spec[0])))
+        axs[i, 1].sharex(axs[0, 0])
+        axs[i, 1].sharey(axs[0, 1])
+        axs[i, 1].axis(ymax=freq_max)
+        axs[i, 1].set_title(f'{data[i].name}, spectrogram')
+        axs[i, 1].set_xlabel('Time [s]')
+        axs[i, 1].set_ylabel('Frequency [Hz]')
+        axs[i, 1].plot(sharex=axs[0, 0])
+
+        """FFT"""
+        axs[i, 2].set_xlim(left=0, right=freq_max)
+        data_fft = scipy.fft.fft(data[i].values, axis=0)
+        fftfreq = scipy.fft.fftfreq(len(data_fft),  1 / SAMPLE_RATE)
+        data_fft = np.fft.fftshift(data_fft)[len(data[i]) // 2:]
+        fftfreq = np.fft.fftshift(fftfreq)[len(data[i]) // 2:]
+        axs[i, 2].sharex(axs[0, 2])
+        axs[i, 2].sharey(axs[0, 2])
+        axs[i, 2].grid()
+        axs[i, 2].set_title(f'{data[i].name}, FFT')
+        axs[i, 2].set_xlabel("Frequency [Hz]")
+        axs[i, 2].set_ylabel("Amplitude [dB]")
+        axs[i, 2].plot(fftfreq, 20 * np.log10(np.abs(data_fft)))
+        # data_fft_phase = data_fft
+        # data_fft_phase[data_fft_phase < 0.1] = 0
+        # plt.plot(fftfreq, (np.angle( data_fft_phase, deg=True)))
+
+    """Adjust to look nice in fullscreen view"""
+    plt.subplots_adjust(left=0.06, right=0.985,
+                        top=0.97, bottom=0.06,
+                        hspace=0.3, wspace=0.2)
+    # plt.show()
+
+
 def plot_fft(df, window=False):
     if isinstance(df, pd.DataFrame):
         if window:
@@ -80,7 +143,7 @@ def plot_spectogram(df,
                     include_signal=True,
                     channel='Sensor 1',
                     freq_max=None):
-    vmin = 10 * np.log10(np.max(df)) - 60
+    vmin = 10 * np.log10(np.max(df)) - 100
     if include_signal:
         time_axis = np.linspace(0, len(df) // SAMPLE_RATE, num=len(df))
         ax1 = plt.subplot(211)
@@ -99,66 +162,6 @@ def plot_spectogram(df,
         plt.xlabel('Time [s]')
         plt.ylabel('Frequency')
     plt.show()
-
-
-def compare_signals(fig, axs,
-                    data: list,
-                    freq_max: int = 40000,
-                    nfft: int = 256):
-    """Visually compare two signals, by plotting:
-    time signal, spectogram, fft and (optionally) difference signal
-    """
-    for i, channel in enumerate(data):
-        if isinstance(data[i], np.ndarray):
-            data[i] = pd.Series(data[i], name='Sensor ' + str(i + 1))
-        """Time signal"""
-        time_axis = np.linspace(start=0,
-                                stop=len(data[i]) / SAMPLE_RATE,
-                                num=len(data[i]))
-        axs[i, 0].sharex(axs[0, 0])
-        axs[i, 0].grid()
-        axs[i, 0].plot(time_axis, data[i])
-        axs[i, 0].set_title(f'{data[i].name}, time signal')
-        axs[i, 0].set_xlabel('Time [s]')
-        axs[i, 0].set_ylabel('Amplitude [V]')
-        axs[i, 0].plot()
-
-        """Spectrogram"""
-        spec = axs[i, 1].specgram(data[i],
-                                  Fs=SAMPLE_RATE,
-                                  NFFT=nfft,
-                                  noverlap=(nfft // 2))
-        # fig.colorbar(spec[3], ax=axs[i, 1])
-        spec[3].set_clim(10 * np.log10(np.max(spec[0])) - 60,
-                         10 * np.log10(np.max(spec[0])))
-        axs[i, 1].sharex(axs[0, 0])
-        axs[i, 1].axis(ymax=freq_max)
-        axs[i, 1].set_title(f'{data[i].name}, spectrogram')
-        axs[i, 1].set_xlabel('Time [s]')
-        axs[i, 1].set_ylabel('Frequency [Hz]')
-        axs[i, 1].plot(sharex=axs[0, 0])
-
-        """FFT"""
-        axs[i, 2].set_xlim(left=0, right=freq_max)
-        data_fft = scipy.fft.fft(data[i].values, axis=0)
-        fftfreq = scipy.fft.fftfreq(len(data_fft),  1 / SAMPLE_RATE)
-        data_fft = np.fft.fftshift(data_fft)[len(data[i]) // 2:]
-        fftfreq = np.fft.fftshift(fftfreq)[len(data[i]) // 2:]
-        axs[i, 2].sharex(axs[0, 2])
-        axs[i, 2].grid()
-        axs[i, 2].set_title(f'{data[i].name}, FFT')
-        axs[i, 2].set_xlabel("Frequency [Hz]")
-        axs[i, 2].set_ylabel("Amplitude [dB]")
-        axs[i, 2].plot(fftfreq, 20 * np.log10(np.abs(data_fft)))
-        # data_fft_phase = data_fft
-        # data_fft_phase[data_fft_phase < 0.1] = 0
-        # plt.plot(fftfreq, (np.angle( data_fft_phase, deg=True)))
-
-    """Adjust to look nice in fullscreen view"""
-    plt.subplots_adjust(left=0.06, right=0.985,
-                        top=0.97, bottom=0.06,
-                        hspace=0.3, wspace=0.2)
-    # plt.show()
 
 
 def specgram_with_lines(setup, measurements_comp, arrival_times, bandwidth):
