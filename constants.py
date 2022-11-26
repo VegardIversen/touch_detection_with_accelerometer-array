f7946f6eb733f2d127b681c95ac78577e2b90900
Niklas Strømsnes <niklas.stromsnes@revolve.no>
2022-11-26 18:58:42 +0100

    Interpolation factor as part of settings

--- constants.py
+++ constants.py
@@ -1,7 +1,8 @@
 """GLOBAL CONSTANTS"""
 import numpy as np
 
-"""Measurement settings"""
-SAMPLE_RATE = 150000
+"""Settings"""
+INTERP_FACTOR = 4
+SAMPLE_RATE = 150000 * INTERP_FACTOR
 CHANNEL_NAMES = np.array(['Sensor 1', 'Sensor 2', 'Sensor 3'])
 CHIRP_CHANNEL_NAMES = np.array(['Actuator', 'Sensor 1', 'Sensor 2', 'Sensor 3'])
