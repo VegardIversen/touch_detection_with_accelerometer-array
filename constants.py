0e03a274dc3372d3ee012c98e946d0ebca6dda3f
Niklas Strømsnes <niklast@stud.ntnu.no>
2022-12-05 18:26:07 +0100

    Interpolate 8 times

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 """Settings"""
-INTERP_FACTOR = 4
+INTERP_FACTOR = 8
 SAMPLE_RATE = 150000 * INTERP_FACTOR
 CHANNEL_NAMES = np.array(['Sensor 1', 'Sensor 2', 'Sensor 3'])
 CHIRP_CHANNEL_NAMES = np.array(['Actuator', 'Sensor 1', 'Sensor 2', 'Sensor 3'])
