e3fa710aab076da718d888f8feb8feded10fa1e1
Niklas Strømsnes <niklast@stud.ntnu.no>
2022-12-09 11:06:21 +0100

    Change constants.py name to be more descriptive

--- constants.py
+++
@@ -1,8 +0,0 @@
-"""GLOBAL CONSTANTS"""
-import numpy as np
-
-"""Settings"""
-INTERPOLATION_FACTOR = 8
-SAMPLE_RATE = 150000 * INTERPOLATION_FACTOR
-CHANNEL_NAMES = ['Sensor 1', 'Sensor 2', 'Sensor 3']
-CHIRP_CHANNEL_NAMES = ['Actuator', 'Sensor 1', 'Sensor 2', 'Sensor 3']
--- generate_results.py
+++ generate_results.py
@@ -7,7 +7,10 @@ import numpy as np
 import pandas as pd
 import scipy.signal as signal
 
-from constants import CHIRP_CHANNEL_NAMES, SAMPLE_RATE, INTERPOLATION_FACTOR
+from global_constants import (CHIRP_CHANNEL_NAMES,
+                              SAMPLE_RATE,
+                              FIGSIZE_ONE_SIGNAL,
+                              FIGSIZE_ONE_COLUMN)
 from csv_to_df import csv_to_df
 from data_processing.detect_echoes import (get_hilbert,
                                            get_travel_times)
@@ -24,13 +27,66 @@ from data_viz_files.visualise_data import (compare_signals,
                                            wave_statistics,
                                            set_fontsizes,
                                            envelopes_with_lines)
-from setups import Setup
+from setups import (Setup,
+                    Setup3_2_without_sensor2,
+                    Setup7)
 
 
-def plot_time_signals(measurements: pd.DataFrame,
-                      signal_start_seconds: float,
-                      signal_length_seconds: float,
-                      FIGSIZE: tuple) -> None:
+"""Setup 3_2"""
+
+
+def results_setup3_2():
+    """Run some general commands for all functions:
+        - Choose file and open it
+        - Channel selection
+        - Choose setup and draw it
+        - Interpolation
+        - Generate results from functions
+    """
+    """Choose file"""
+    FILE_FOLDER = 'prop_speed_files/setup3_2'
+    FILE_NAME = 'prop_speed_chirp3_setup3_2_v1'
+    """Open file"""
+    measurements = csv_to_df(file_folder=FILE_FOLDER,
+                             file_name=FILE_NAME)
+
+    """Delete sensor 2 as it doesn't have the required bandwidth"""
+    measurements = measurements.drop(['Sensor 2'], axis='columns')
+    CHIRP_CHANNEL_NAMES.remove('Sensor 2')
+
+    """Choose setup"""
+    SETUP = Setup3_2_without_sensor2()
+    """Draw setup"""
+    SETUP.draw()
+
+    """Pyplot adjustments"""
+    set_fontsizes()
+
+    """Interpolate waveforms"""
+    measurements = interpolate_waveform(measurements)
+
+    """Set everything but the signal to zero"""
+    signal_length_seconds = 2 + 0.05  # Length of chirp + time for sensor 3 to die down
+    threshold = 0.001  # Determine empirically
+    measurements, signal_start_seconds = window_signals(measurements,
+                                                        signal_length_seconds,
+                                                        threshold)
+
+    """Run functions to generate results"""
+    plot_time_signals_setup3_2(measurements,
+                               signal_start_seconds,
+                               signal_length_seconds)
+    plot_spectrogram_signals_setup3_2(measurements,
+                                      signal_start_seconds,
+                                      signal_length_seconds)
+    plot_fft_signals_setup_3_2(measurements,
+                               signal_start_seconds,
+                               signal_length_seconds)
+
+
+def plot_time_signals_setup3_2(measurements: pd.DataFrame,
+                               signal_start_seconds: float,
+                               signal_length_seconds: float) -> None:
     """SETTINGS FOR PLOTTING"""
     plots_to_plot = ['time']
     subplots_adjust = {'left': 0.125, 'right': 0.965,
@@ -40,7 +96,7 @@ def plot_time_signals(measurements: pd.DataFrame,
     """Plot the raw measurements"""
     fig, axs = plt.subplots(nrows=measurements.shape[1],
                             ncols=len(plots_to_plot),
-                            figsize=FIGSIZE,
+                            figsize=FIGSIZE_ONE_COLUMN,
                             squeeze=False)
     compare_signals(fig, axs,
                     [measurements[channel] for channel in measurements],
@@ -63,7 +119,7 @@ def plot_time_signals(measurements: pd.DataFrame,
     """Plot the measurements"""
     fig, axs = plt.subplots(nrows=measurements.shape[1],
                             ncols=len(plots_to_plot),
-                            figsize=FIGSIZE,
+                            figsize=FIGSIZE_ONE_COLUMN,
                             squeeze=False)
     compare_signals(fig, axs,
                     [measurements[channel] for channel in measurements],
@@ -85,10 +141,9 @@ def plot_time_signals(measurements: pd.DataFrame,
                         wspace=subplots_adjust['wspace'])
 
 
-def plot_spectrogram_signals(measurements: pd.DataFrame,
-                             signal_start_seconds: float,
-                             signal_length_seconds: float,
-                             FIGSIZE: tuple) -> None:
+def plot_spectrogram_signals_setup3_2(measurements: pd.DataFrame,
+                                      signal_start_seconds: float,
+                                      signal_length_seconds: float) -> None:
     """SETTINGS FOR PLOTTING"""
     NFFT = 1024
     plots_to_plot = ['spectrogram']
@@ -99,7 +154,7 @@ def plot_spectrogram_signals(measurements: pd.DataFrame,
     """Plot the raw measurements"""
     fig, axs = plt.subplots(nrows=measurements.shape[1],
                             ncols=len(plots_to_plot),
-                            figsize=FIGSIZE,
+                            figsize=FIGSIZE_ONE_COLUMN,
                             squeeze=False)
     compare_signals(fig, axs,
                     [measurements[channel] for channel in measurements],
@@ -123,7 +178,7 @@ def plot_spectrogram_signals(measurements: pd.DataFrame,
     """Plot the measurements"""
     fig, axs = plt.subplots(nrows=measurements.shape[1],
                             ncols=len(plots_to_plot),
-                            figsize=FIGSIZE,
+                            figsize=FIGSIZE_ONE_COLUMN,
                             squeeze=False)
     compare_signals(fig, axs,
                     [measurements[channel] for channel in measurements],
@@ -142,10 +197,9 @@ def plot_spectrogram_signals(measurements: pd.DataFrame,
                         wspace=subplots_adjust['wspace'])
 
 
-def plot_fft_signals(measurements: pd.DataFrame,
-                     signal_start_seconds: float,
-                     signal_length_seconds: float,
-                     FIGSIZE: tuple) -> None:
+def plot_fft_signals_setup_3_2(measurements: pd.DataFrame,
+                               signal_start_seconds: float,
+                               signal_length_seconds: float) -> None:
     """SETTINGS FOR PLOTTING"""
     plots_to_plot = ['fft']
     subplots_adjust = {'left': 0.125, 'right': 0.95,
@@ -155,7 +209,7 @@ def plot_fft_signals(measurements: pd.DataFrame,
     """Plot the raw measurements"""
     fig, axs = plt.subplots(nrows=measurements.shape[1],
                             ncols=len(plots_to_plot),
-                            figsize=FIGSIZE,
+                            figsize=FIGSIZE_ONE_COLUMN,
                             squeeze=False)
     compare_signals(fig, axs,
                     [measurements[channel] for channel in measurements],
@@ -182,7 +236,7 @@ def plot_fft_signals(measurements: pd.DataFrame,
     """Plot the measurements"""
     fig, axs = plt.subplots(nrows=measurements.shape[1],
                             ncols=len(plots_to_plot),
-                            figsize=FIGSIZE,
+                            figsize=FIGSIZE_ONE_COLUMN,
                             squeeze=False)
     compare_signals(fig, axs,
                     [measurements[channel] for channel in measurements],
@@ -202,6 +256,58 @@ def plot_fft_signals(measurements: pd.DataFrame,
     """Limit y axis"""
     for ax in axs.flatten():
         ax.set_ylim(bottom=-50, top=230)
+
+
+"""Setup 7"""
+
+
+def results_setup7():
+    """Run some general commands for all functions:
+        - Choose file and open it
+        - Channel selection
+        - Choose setup and draw it
+        - Interpolation
+        - Generate results from functions
+    """
+    """CONFIG"""
+    FILE_FOLDER = 'setup7'
+    FILE_NAME = 'notouchThenHoldB2_20to40khz_125ms_10vpp_v1'
+    SETUP = Setup7()
+    TIME_START = 0.114  # s
+    TIME_END = 0.246  # s
+
+    set_fontsizes()
+
+    SETUP.draw()
+
+    """Open file"""
+    measurements = csv_to_df(file_folder=FILE_FOLDER,
+                             file_name=FILE_NAME,
+                             channel_names=CHIRP_CHANNEL_NAMES)
+
+    """Crop data"""
+    # measurements = crop_data(measurements,
+    #                          time_start=TIME_START,
+    #                          time_end=TIME_END)
+
+    plot_raw_time_signal_setup7(measurements)
+    plt.show()
+
+
+def plot_raw_time_signal_setup7(measurements: pd.DataFrame):
+    """Plot raw signal"""
+    time_axis = np.linspace(start=0,
+                            stop=1000 * len(measurements) / SAMPLE_RATE,
+                            num=len(measurements))
+    fig, ax = plt.subplots(nrows=1,
+                           ncols=1,
+                           figsize=FIGSIZE_ONE_SIGNAL)
+    ax.set_title('Chirp from 20 khz to 40 kHz in 125 ms')
+    ax.plot(time_axis, 1000 * measurements['Sensor 1'], label='Sensor 1')
+    ax.set_xlabel('Time (ms)')
+    ax.set_ylabel('Amplitude (mV)')
+    ax.legend()
+    ax.grid()
 
 
 if __name__ == '__main__':
