2ddd56853399f05c726c800b6c6f65b6e0cee016
Niklas Strømsnes <niklast@stud.ntnu.no>
2022-12-18 12:56:29 +0100

    Add path for saving figures

--- global_constants.py
+++ global_constants.py
@@ -9,6 +9,7 @@ CHIRP_CHANNEL_NAMES = ['Actuator', 'Sensor 1', 'Sensor 2', 'Sensor 3']
 """Figure sizes"""
 FIGSIZE_ONE_SIGNAL = (8, 4)
 FIGSIZE_ONE_COLUMN = (8, 9)
+FIGSIZE_TWO_COLUMNS = (16, 9)
 FIGSIZE_THREE_COLUMNS = (20, 9)
 
 """Sensor int values, primarily for use with sensors[SENSOR_*]"""
@@ -16,3 +17,6 @@ ACTUATOR_1 = 0
 SENSOR_1 = 0
 SENSOR_2 = 1
 SENSOR_3 = 2
+
+"""Saving figures"""
+SAVE_FIGURES_PATH = '/home/niklast/Documents/GitHub/Specialization-Project-LaTeX/Figures/'
