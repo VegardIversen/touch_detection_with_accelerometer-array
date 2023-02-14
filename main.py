99f89ababcfb230da96a4c0d4d6b4628f9fba2a6
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-02-14 15:35:39 +0100

    Changing to black formatter

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ Date: 2022-01-09
 """
 
 import matplotlib.pyplot as plt
-from main_scripts.project_thesis import (setup1_results,
-                                         setup2_results,
-                                         setup3_results,
-                                         custom_plots)
-from main_scripts.bandpassing_touch import (setup1_predict_reflections)
-from main_scripts.dispersion_compensation import (correct_dispersion,
-                                                  dispersive_filter)
+from main_scripts.project_thesis import (
+    setup1_results,
+    setup2_results,
+    setup3_results,
+    custom_plots,
+)
+from main_scripts.bandpassing_touch import setup1_predict_reflections
+from main_scripts.dispersion_compensation import correct_dispersion, dispersive_filter
 from utils.data_visualization.visualize_data import set_fontsizes
 
 
@@ -20,29 +21,31 @@ def main():
     """
     set_fontsizes()
 
-    while input not in ['0', '1', '2', '3']:
-        print('\nWhich setup do you want to generate results for?')
-        print('1: Setup 1')
-        print('2: Setup 2')
-        print('3: Setup 3')
-        print('0: Dev')
+    while input not in ["0", "1", "2", "3"]:
+        print("\nWhich setup do you want to generate results for?")
+        print("1: Setup 1")
+        print("2: Setup 2")
+        print("3: Setup 3")
+        print("0: Dev")
         # user_input = input('Enter number: ')
-        user_input = '0'
-        if user_input == '1':
+        user_input = "0"
+        if user_input == "1":
             setup1_results()
-        elif user_input == '2':
+        elif user_input == "2":
             setup2_results()
-        elif user_input == '3':
+        elif user_input == "3":
             setup3_results()
-        elif user_input == '0':
+        elif user_input == "0":
             correct_dispersion()
             plt.show()
         else:
-            print('Please type 1, 2 or 3 for their respective setups '
-                  'or 0 for code currently under development.')
+            print(
+                "Please type 1, 2 or 3 for their respective setups "
+                "or 0 for code currently under development."
+            )
         plt.show()
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
