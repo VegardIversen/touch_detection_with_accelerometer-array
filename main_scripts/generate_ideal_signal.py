96100630d3d687d24d220b466198b47521ef5721
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-03-02 08:16:08 +0100

    Rename to ideal signal

--- main_scripts/generate_ideal_signal.py
+++ main_scripts/generate_ideal_signal.py
@@ -2,71 +2,95 @@ import numpy as np
 import pandas as pd
 import scipy.signal as signal
 import matplotlib.pyplot as plt
+from utils.data_processing.processing import align_signals_by_max_value, normalize
 
 from utils.plate_setups import Setup
-from utils.chirp_gen import generate_chirp_and_save_to_file
+from utils.chirp_gen import generate_chirp
 from utils.global_constants import (SAMPLE_RATE,
                                     ACTUATOR_1,
                                     SENSOR_1,)
 from utils.data_visualization.visualize_data import (
-    compare_signals, set_window_size, envelope_with_lines, adjust_plot_margins)
-from utils.data_processing.detect_echoes import get_travel_times
+    compare_signals, set_window_size, adjust_plot_margins)
+from utils.data_processing.detect_echoes import get_envelopes, get_travel_times
 from utils.data_processing.preprocessing import (
     compress_chirps, crop_measurement_to_signal)
 
 
-def generate_test_signal(setup: Setup, propagation_speed_mps: float = 600):
+def compare_to_ideal_signal(setup: Setup, measurements: pd.DataFrame):
+    """Calculate arrival times for sensor 1"""
+    propagation_speed = setup.get_propagation_speed(measurements)
+    print(f'Propagation speed: {propagation_speed}')
+    ideal_signal = generate_ideal_signal(setup, propagation_speed)
+
+    measurement_envelopes = get_envelopes(measurements)
+    measurement_envelopes = normalize(measurement_envelopes)
+    ideal_signal = align_signals_by_max_value(
+        signals=ideal_signal, signals_to_align_with=measurement_envelopes)
+    """Plot signals"""
+    CHANNELS_TO_PLOT = [measurements.columns]
+    PLOTS_TO_PLOT = ['time', 'fft']
+    fig, axs = plt.subplots(nrows=len(CHANNELS_TO_PLOT),
+                            ncols=len(PLOTS_TO_PLOT),
+                            squeeze=False)
+    compare_signals(fig, axs,
+                    [ideal_signal['Sensor 1'],
+                     ideal_signal['Sensor 3']],
+                    plots_to_plot=PLOTS_TO_PLOT)
+    compare_signals(fig, axs,
+                    [measurement_envelopes['Sensor 1'],
+                     measurement_envelopes['Sensor 3'],],
+                    plots_to_plot=PLOTS_TO_PLOT)
+    axs[0, 0].grid()
+    axs[0, 0].legend(['Ideal signal', 'Measurement envelope'],
+                     loc='upper right')
+    axs[1, 0].grid()
+    axs[0, 1].grid()
+    axs[0, 1].legend(['Ideal signal', 'Measurement envelope'],
+                     loc='upper right')
+    axs[1, 1].grid()
+    return 0
+
+
+def generate_ideal_signal(setup: Setup,
+                          propagation_speed_mps: float,
+                          attenuation_dBpm: float,
+                          time_end: float,
+                          frequency_start: float,
+                          frequency_stop: float,):
     """Generate a test signal based on expected arrival times for a setup."""
 
-    setup.draw()
-    adjust_plot_margins()
+    # setup.draw()
+    # adjust_plot_margins()
 
     # Generate a chirp for transmission
-    TIME_END = 0.125
-    FREQUENCY_START = 3_000
-    FREQUENCY_STOP = 40_000
-    chirp = make_chirp(TIME_END, FREQUENCY_START, FREQUENCY_STOP)
+
+    chirp = make_chirp(time_end, frequency_start, frequency_stop)
 
     # Initialize the superpositioned signal
     sensor_measurements = sum_signals(
-        setup, propagation_speed_mps, chirp, attenuation_dBpm=20)
+        setup, propagation_speed_mps, chirp, attenuation_dBpm)
 
     # Compress the test signal
-    compressed_test_signal = compress_chirps(sensor_measurements)
-    crop_measurement_to_signal(compressed_test_signal, custom_threshold=3000)
-
-    # Plot compressed test signal
-    PLOTS_TO_PLOT = ['time', 'fft']
-    fig, axs = plt.subplots(4, len(PLOTS_TO_PLOT), squeeze=False)
-    compare_signals(fig, axs,
-                    data=[compressed_test_signal['Actuator'],
-                          compressed_test_signal['Sensor 1'],
-                          compressed_test_signal['Sensor 2'],
-                          compressed_test_signal['Sensor 3']],
-                    compressed_chirps=True,
-                    nfft=2**4,
-                    plots_to_plot=PLOTS_TO_PLOT,)
-
-    return compressed_test_signal
+    compressed_ideal_signal = compress_chirps(sensor_measurements)
+    crop_measurement_to_signal(compressed_ideal_signal, custom_threshold=3000)
+    return compressed_ideal_signal
 
 
-def make_chirp(TIME_END: float,
-               FREQUENCY_START: float,
-               FREQUENCY_STOP: float,
+def make_chirp(time_end: float,
+               frequency_start: float,
+               frequency_stop: float,
                plot_chirp: bool = False):
-    chirp = generate_chirp_and_save_to_file(sample_rate=SAMPLE_RATE,
-                                            frequency_start=FREQUENCY_START,
-                                            frequency_stop=FREQUENCY_STOP,
-                                            time_end=TIME_END,
-                                            save_to_file=False)
-
+    chirp = generate_chirp(sample_rate=SAMPLE_RATE,
+                           frequency_start=frequency_start,
+                           frequency_stop=frequency_stop,
+                           time_end=time_end,
+                           save_to_file=False)
     if plot_chirp:
         # Plot the chirp
         fig, axs = plt.subplots(1, 3, squeeze=False,
                                 figsize=set_window_size(rows=1, cols=3))
         compare_signals(fig, axs,
                         data=[chirp],)
-
     return chirp
 
 
@@ -87,10 +111,10 @@ def sum_signals(setup: Setup,
         arrival_times, _ = get_travel_times(setup.actuators[ACTUATOR_1],
                                             setup.sensors[sensor_i],
                                             propagation_speed_mps,
+                                            surface=setup.surface,
                                             milliseconds=False,
                                             relative_first_reflection=False,
                                             print_info=False)
-
         for arrival_time in arrival_times:
             # Calculate the index of the arrival time
             arrival_time_index = int(arrival_time * SAMPLE_RATE)
@@ -100,7 +124,6 @@ def sum_signals(setup: Setup,
             measurement_i[arrival_time_index:arrival_time_index +
                           len(chirp)] += chirp * 10**(-attenuation_dBpm *
                                                       travel_distance_m / 20)
-
         sensor_measurements[f'Sensor {sensor_i + 1}'] = measurement_i
 
     # Plot test signal
@@ -113,7 +136,6 @@ def sum_signals(setup: Setup,
                               sensor_measurements['Sensor 2'],
                               sensor_measurements['Sensor 3']],
                         plots_to_plot=PLOTS_TO_PLOT,)
-
     return sensor_measurements
 
 
