d2c53ce760cc6f3eea76d513eae047a215d64ea8
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-03-02 08:59:50 +0100

    Remove setup in drawing.py

--- utils/data_visualization/drawing.py
+++ utils/data_visualization/drawing.py
@@ -8,8 +8,8 @@ import matplotlib.patches as patches
 from collections import OrderedDict
 
 from utils.data_processing.detect_echoes import (find_mirrored_source,
-                                           flip_sensors,
-                                           flip_sources)
+                                                 flip_sensors,
+                                                 flip_sources)
 from utils.objects import MirroredSensor, MirroredSource, Table, Actuator, Sensor
 
 
@@ -37,42 +37,4 @@ def plot_legend_without_duplicates_ax(ax):
     ax.legend(by_label.values(), by_label.keys(), loc='upper right')
 
 
-def draw_setup_example():
-    table = Table()
-
-    sensor_2 = Sensor(coordinates=np.array([table.LENGTH / 2,
-                                            table.WIDTH - 0.05]))
-    sensor_1 = Sensor(coordinates=(sensor_2.x - 0.02, sensor_2.y))
-    sensor_3 = Sensor(coordinates=(sensor_2.x + 0.02, sensor_2.y))
-    sensor_4 = Sensor(coordinates=(sensor_2.x - 0.02, sensor_2.y - 0.02))
-    sensor_5 = Sensor(coordinates=(sensor_2.x, sensor_2.y - 0.02))
-    sensor_6 = Sensor(coordinates=(sensor_2.x + 0.02, sensor_2.y - 0.02))
-    sensor_7 = Sensor(coordinates=(sensor_2.x - 0.02, sensor_2.y - 0.04))
-    sensor_8 = Sensor(coordinates=(sensor_2.x, sensor_2.y - 0.04))
-    sensor_9 = Sensor(coordinates=(sensor_2.x + 0.02, sensor_2.y - 0.04))
-
-    actuator = Actuator(coordinates=np.array([sensor_5.x - 0.10,
-                                              sensor_5.y]))
-
-    plt.axes()
-    table.draw()
-    actuator.draw()
-    sensor_2.draw()
-    sensor_1.draw()
-    sensor_3.draw()
-    sensor_4.draw()
-    sensor_5.draw()
-    sensor_6.draw()
-    sensor_7.draw()
-    sensor_8.draw()
-    sensor_9.draw()
-
-    plt.axis('scaled')
-    plt.xlabel('x (m)')
-    plt.ylabel('y (m)')
-    plot_legend_without_duplicates()
-
-    plt.show()
-
     return actuator, sensor_1, sensor_2, sensor_3
-
