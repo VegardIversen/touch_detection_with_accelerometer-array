2f9089495489a61e3f9c1023fee2e4ee825ea16d
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-03-03 15:37:34 +0100

    Slightly modify chirp_gen

--- utils/chirp_gen.py
+++ utils/chirp_gen.py
@@ -9,19 +9,19 @@ import numpy as np
 def generate_chirp(sample_rate=150000,
                    frequency_start=20000,
                    frequency_stop=40000,
-                   time_end=0.125,
+                   time_end_s=0.125,
                    chirp_method='linear',
-                   save_to_file=True):
-    print(f"Fs: {sample_rate}, freq_start: {frequency_start}, freq_stop: {frequency_stop}, t_max: {time_end} s")
+                   save_to_file=False):
+    print(f"Fs: {sample_rate}, freq_start: {frequency_start}, freq_stop: {frequency_stop}, t_max: {time_end_s} s")
 
-    time_axis = np.linspace(0, time_end, int(time_end * sample_rate))
+    time_axis = np.linspace(0, time_end_s, int(time_end_s * sample_rate))
     generated_chirp = signal.chirp(t=time_axis,
                                    f0=frequency_start,
-                                   t1=time_end,
+                                   t1=time_end_s,
                                    f1=frequency_stop,
                                    method=chirp_method)
     if save_to_file:
-        np.savetxt(f"chirp_{sample_rate}Hz_{time_end}s_{frequency_start}Hz-{frequency_stop}Hz_{chirp_method}.csv",
+        np.savetxt(f"chirp_{sample_rate}Hz_{time_end_s}s_{frequency_start}Hz-{frequency_stop}Hz_{chirp_method}.csv",
                    generated_chirp,
                    delimiter=",")
 
