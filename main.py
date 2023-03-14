f2c0fefb6b9e8c28e6458a1761acc23ec0c6d364
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-03-14 14:43:21 +0100

    Remove main.py terminal menu not used at all atm

--- main.py
+++ main.py
@@ -4,37 +4,16 @@ Date: 2022-01-09
 
 import matplotlib.pyplot as plt
 from main_scripts.linear_array_by_edge import linear_array_by_edge
-from main_scripts.project_thesis import (setup1_results,
-                                         setup2_results,
-                                         setup3_results,)
+# from main_scripts.project_thesis import (setup1_results,
+#                                          setup2_results,
+#                                          setup3_results,)
 from utils.data_visualization.visualize_data import set_fontsizes
 
 
 def main():
-    """Run functions from generate_results.py
-    NOTE:   Individual figures can be plotted by wrapping it with
-            plt.close('all') and plt.show()
-    """
     set_fontsizes()
 
-    print('\nWhich setup do you want to generate results for?')
-    print('1: Setup 1')
-    print('2: Setup 2')
-    print('3: Setup 3')
-    print('0: Dev')
-    # user_input = input('Enter number: ')
-    user_input = '0'
-    if user_input == '1':
-        setup1_results()
-    elif user_input == '2':
-        setup2_results()
-    elif user_input == '3':
-        setup3_results()
-    elif user_input == '0':
-        linear_array_by_edge()
-    else:
-        print('Please type 1, 2 or 3 for their respective setups '
-              'or 0 for code currently under development.')
+    linear_array_by_edge()
     plt.show()
 
     return 0
