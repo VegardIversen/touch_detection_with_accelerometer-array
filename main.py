7883ffd27adc45973be9e979ef76ed7baca84fe2
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-03-18 13:55:42 +0100

    Inspect setup 4 touch and release

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ Date: 2022-01-09
 
 import matplotlib.pyplot as plt
 
-from main_scripts.inspect_10mm_plate_touch import inspect_touch
-from main_scripts.linear_array_by_edge import linear_array_by_edge
+from main_scripts.inspect_touch_release import inspect_touch_release
 from utils.data_visualization.visualize_data import set_fontsizes
 
 
 def main():
     set_fontsizes()
-    inspect_touch()
+    inspect_touch_release()
     plt.show()
 
     return 0
