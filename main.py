a540b5837435aba34fa00c67a4c223471c722208
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-04-23 13:00:06 +0200

    Add main script for finding propagation speed

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ Date: 2022-01-09
 """
 
 import matplotlib.pyplot as plt
-from main_scripts.generate_signals_for_matlab import generate_signals_for_matlab
-
+from main_scripts.find_propagation_velocities import find_propagation_velocities
 from utils.data_visualization.visualize_data import set_fontsizes
 
 
 def main():
     set_fontsizes()
     # Call one of the functions found in /main_scripts
-    generate_signals_for_matlab()
+    find_propagation_velocities()
     plt.show()
-
     return 0
 
 
