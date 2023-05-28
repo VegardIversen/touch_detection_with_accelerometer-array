4c212b651b70832fc3b84005c8868832eb5940ab
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-05-28 19:07:33 +0200

    Function for converting from dB to linear

--- utils/little_helpers.py
+++ utils/little_helpers.py
@@ -2,14 +2,22 @@ import numpy as np
 import pandas as pd
 
 
-def to_dB(input: pd.DataFrame or np.ndarray or float):
+def to_dB(input_signal: pd.DataFrame or np.ndarray or float):
     """Converts measurements to dB"""
-    if isinstance(input, float):
-        input = np.array([input])
+    if isinstance(input_signal, float):
+        input_signal = np.array([input_signal])
     # Avoid division by zero
-    input[input == 0] = 1e-10
-    measurements_dB = 20 * np.log10(input)
+    input_signal[input_signal == 0] = 1e-10
+    measurements_dB = 20 * np.log10(input_signal)
     return measurements_dB
+
+
+def to_linear(input_signals: pd.DataFrame or np.ndarray or float):
+    """Converts measurements in dB to linear"""
+    if isinstance(input_signals, float):
+        input_signals = np.array([input_signals])
+    measurements_linear = 10 ** (input_signals / 20)
+    return measurements_linear
 
 
 def distance_between(object1_coordinates: np.ndarray, object2_coordinates: np.ndarray):
